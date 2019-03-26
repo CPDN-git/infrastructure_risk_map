000680e4c747a08e0ce4311e8f045b50808ef986
Joseph Burns <48716924+Jobbalob@users.noreply.github.com>
2019-03-26 12:02:35 +0000

    Calc risk ratios for every grid cell, for given threshold

---
+++ risk_ratio.py
@@ -0,0 +1,71 @@
+###### Making risk ratios for each grid cell, storing them
+###### in dataframe ready to be added to grid
+
+import numpy as np
+import pandas as pd
+
+
+from find_threshold_prob import prob_below 
+from find_threshold_prob import prob_above 
+
+def cell_risk_ratio(data,dataHist,lat_index,long_index,threshold,below):
+	if below == 'below':
+		P=prob_below(data[:,:,:,lat_index,long_index],threshold)
+		print P
+		PHist=prob_below(dataHist[:,:,:,lat_index,long_index],threshold)
+		print PHist
+		if PHist==0:
+			if P == PHist:
+				ratio=1
+			else:
+				PHist=-1.8*10**-11
+				ratio=float(P/PHist)
+		else:
+			ratio=float(P/PHist)
+		return ratio
+	else:
+		P=prob_above(data[:,:,:,lat_index,long_index],threshold)
+		print P
+		PHist=prob_above(dataHist[:,:,:,lat_index,long_index],threshold)
+		print PHist
+		if PHist==0:
+			if P == PHist:
+				ratio=1
+			else:
+				PHist=-1.8*10**-11
+				ratio=P/PHist
+		else:
+			ratio=float(P/PHist)
+		return ratio
+	
+def grid_ratios(batches,dataHist,threshold,below):
+	i=0
+	ratio_array=np.empty(dataHist.shape[3]*dataHist.shape[4])
+	print ratio_array.shape
+	for batch in batches:
+		print('new batch')
+		ratios=[]	
+		for lat_index in range(batch.shape[3]):
+			for long_index in range(batch.shape[4]):
+				ratio=cell_risk_ratio(batch,dataHist,lat_index,long_index,threshold,below)
+				ratios+=[ratio]
+				print ('ratio for ' + str(lat_index) +',' + str(long_index)+
+				 ' = ' + str(ratio))
+		ratios=np.array(ratios)
+		ratio_array=np.column_stack([ratio_array,ratios])
+		
+	return ratio_array
+	
+	
+def main():
+	dataTmaxHist=np.load('/home/users/jburns59/cpdn_analysis/examples/batch_646_data_item3236_daily_maximum_full_field.npy')
+	dataTmax15=np.load('/home/users/jburns59/cpdn_analysis/examples/batch_647_data_item3236_daily_maximum_full_field.npy')
+	dataTmax2=np.load('/home/users/jburns59/cpdn_analysis/examples/batch_648_data_item3236_daily_maximum_full_field.npy')
+	batches=[dataTmax15,dataTmax2]
+	
+	array_of_ratios=grid_ratios(batches,dataTmaxHist,298,'')
+	np.save('risk_ratio_above_298K',array_of_ratios)
+	
+if __name__=='__main__':
+	main()
+	
